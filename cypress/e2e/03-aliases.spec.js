2b136cd12654d9c4b7387bd0571060d0857c7173
Evgenii Serebriakov <evgenii.serebriakov@adjust.com>
2023-12-14 17:52:13 +0100

    chore: implement aliases tests

--- cypress/e2e/03-aliases.spec.js
+++ cypress/e2e/03-aliases.spec.js
@@ -3,5 +3,18 @@
 describe('Aliases', () => {
   beforeEach(() => {
     cy.visit('/jetsetter');
+
+    cy.get('[data-test="items"]').as('allItems');
+    cy.get('[data-test="items-unpacked"]').as('unpackedItems');
+    cy.get('[data-test="items-packed"]').as('packedItems');
+    cy.get('[data-test="filter-items"]').as('filterInput');
+  });
+
+  it('should filter the items shown on the page', () => {
+    cy.get('@filterInput').type('iPhone');
+
+    cy.get('@allItems').find('li').each(($el) => {
+      cy.wrap($el).should('contain.text', 'iPhone')
+    });
   });
 });
