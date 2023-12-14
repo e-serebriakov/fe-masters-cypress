646e6398043ef8af2c1423a04818a36f5582e731
Evgenii Serebriakov <evgenii.serebriakov@adjust.com>
2023-12-14 14:47:09 +0100

    chore: complete first flight tests

--- cypress/e2e/01-first-flight.spec.js
+++ cypress/e2e/01-first-flight.spec.js
@@ -1,3 +1,20 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+  beforeEach(() => {
+    cy.visit('/jetsetter');
+  })
+
+  it('should', () => {
+    cy.get('form').should('exist')
+  })
+
+  it('should have the words "Add Item"', () => {
+    cy.get('form').should('contain', 'Add Item')
+  })
+
+  it('should out stuff in an input field', () => {
+    cy.get('[data-test="new-item-input"]').type('Good attitude')
+    cy.get('[data-test="add-item"]').click();
+  })
+});
