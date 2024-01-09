5c0a91425f8dc2344ea8b355a311cf0f4b710aba
Evgenii Serebriakov <evgenii.serebriakov@adjust.com>
2024-01-09 16:52:13 +0100

    chore: implement secret menu tests

--- cypress/e2e/06-secret-menu.spec.js
+++ cypress/e2e/06-secret-menu.spec.js
@@ -39,4 +39,26 @@ describe('Secret Menu Items', () => {
   it('should exist have the title on the page', () => {
     cy.get('h1').should('contain', 'Secret Menu Items');
   });
+
+  properties.forEach((property) => {
+    it(`should have a column for ${property} property`, () => {
+      cy.get(`#${property}-column`).should('exist');
+    });
+
+    it(`should hide the ${property} column if unchecked`, () => {
+      cy.get(`#${property}-column`).should('exist');
+      cy.get(`#show-${property}`).uncheck();
+      cy.get(`#${property}-column`).should('be.hidden');
+    });
+  });
+
+  ratings.forEach((rating) => {
+    it(`should show items with rating equal or greater than ${rating}`, () => {
+      cy.get('#minimum-rating-visibility').invoke('val', rating).trigger('input');
+
+      cy.get('td[headers="popularity-column"]').each(($el) => {
+        cy.wrap($el).invoke('text').then(parseInt).should('be.a', 'number').and('gte', rating - 1);
+      });
+    });
+  });
 });
