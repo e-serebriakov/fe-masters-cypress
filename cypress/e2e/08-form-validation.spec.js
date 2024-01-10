874d4b04dfd097795428270810eeb5cf1651ddbd
Evgenii Serebriakov <evgenii.serebriakov@adjust.com>
2024-01-10 16:46:03 +0100

    chore: implement form validation tests

--- cypress/e2e/08-form-validation.spec.js
+++ cypress/e2e/08-form-validation.spec.js
@@ -3,11 +3,36 @@
 describe('Sign Up', () => {
   beforeEach(() => {
     cy.visit('/echo-chamber/sign-up');
+    cy.get('[data-test="sign-up-submit"]').as('submitButton');
   });
 
-  it('should require an email', () => {});
+  it('should require an email', () => {
+    cy.get('@submitButton').click();
 
-  it('should require that the email actually be an email address', () => {});
+    cy.get('[data-test="sign-up-email"]:invalid')
+    .invoke('prop', 'validity')
+    .its('valueMissing')
+    .should('be.true');
+  });
 
-  it('should require a password when the email is present', () => {});
+  it('should require that the email actually be an email address', () => {
+    cy.get('[data-test="sign-up-email"]').type('notanemail');
+
+    cy.get('@submitButton').click();
+
+    cy.get('[data-test="sign-up-email"]:invalid')
+    .invoke('prop', 'validity')
+    .its('typeMismatch')
+    .should('be.true');
+  });
+
+  it('should require a password when the email is present', () => {
+    cy.get('[data-test="sign-up-email"]').type('a@a.com');
+    cy.get('@submitButton').click();
+
+    cy.get('[data-test="sign-up-password"]:invalid')
+    .invoke('prop', 'validity')
+    .its('valueMissing')
+    .should('be.true');
+  });
 });
