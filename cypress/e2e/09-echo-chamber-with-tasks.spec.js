4934ce3f04f5130a464aca69c5f7573f6ba753c9
Evgenii Serebriakov <evgenii.serebriakov@adjust.com>
2024-01-10 18:53:08 +0100

    chore: implement commands

--- cypress/e2e/09-echo-chamber-with-tasks.spec.js
+++ cypress/e2e/09-echo-chamber-with-tasks.spec.js
@@ -11,17 +11,8 @@ describe('Sign Up', () => {
   });
   
   it('should successfully create a user when entering an email and a password', () => {
-    // Sign Up
-    cy.visit('/echo-chamber/sign-up');
-    cy.get('[data-test="sign-up-email"]').type(user.email);
-    cy.get('[data-test="sign-up-password"]').type(user.password);
-    cy.get('[data-test="sign-up-submit"]').click();
-
-    // Sign In
-    cy.visit('/echo-chamber/sign-in');
-    cy.get('[data-test="sign-in-email"]').type(user.email);
-    cy.get('[data-test="sign-in-password"]').type(user.password);
-    cy.get('[data-test="sign-in-submit"]').click();
+    cy.command('signUp', user);
+    cy.command('signIn', user);
 
     cy.location('pathname').should('contain', '/echo-chamber/posts');
     cy.contains('Signed in as ' + user.email);
