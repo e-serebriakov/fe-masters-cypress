8bb7d62e8e333c09a664e0284fd8e69fcfa8234e
Evgenii Serebriakov <evgenii.serebriakov@adjust.com>
2024-01-16 16:29:22 +0100

    chore: implement dog facts tests

--- cypress/e2e/12-dog-facts.spec.js
+++ cypress/e2e/12-dog-facts.spec.js
@@ -13,16 +13,41 @@ describe('Dog Facts', () => {
   });
 
   it('should start out with an empty state', () => {
-    cy.get('@emptyState');
+    cy.get('@emptyState').should('exist');
   });
 
-  it('should make a request when the button is called', () => {});
+  it('should make a request when the button is called', () => {
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
+  });
 
-  it('should adjust the amount when the select is changed', () => {});
+  it('should adjust the amount when the select is changed', () => {
+    cy.get('@amountSelect').select('5');
+    cy.get('@amountSelect').should('have.value', '5');
+  });
 
-  it('should show the correct number of facts on the page', () => {});
+  it('should show the correct number of facts on the page', () => {
+    cy.get('@amountSelect').select('5');
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
 
-  it('should clear the facts when the "Clear" button is pressed', () => {});
+    cy.get('[data-test="dog-fact"]').should('have.length', 5);
+  });
 
-  it("should reflect the number of facts we're looking for in the title", () => {});
+  it('should clear the facts when the "Clear" button is pressed', () => {
+    cy.get('@amountSelect').select('5');
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
+
+    cy.get('@clearButton').click();
+    cy.get('@emptyState').should('exist');
+  });
+
+  it("should reflect the number of facts we're looking for in the title", () => {
+    cy.get('@amountSelect').select('5');
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
+
+    cy.title().should('contain', '5');
+  });
 });
